c3c496ba43218564f06443f5088a598deb94bd77
paka95 <patryk.pomierski@gmail.com>
2023-05-04 23:01:27 +0200

    Putting retrieved meal objects in template containers

--- meals/static/meals/js/build-list.js
+++ meals/static/meals/js/build-list.js
@@ -6,13 +6,108 @@ export function buildList() {
     .then(data => {
         const mealsListContainer = document.getElementById('meals-list-container');
         const days = groupByDays(data);
+        console.log("====days====");
         console.log(days);
+        console.log("====days====");
         for (const day in days) {
-            console.log(day);
+            if (Object.keys(days[day]).length !== 0) {
+
+            console.log("day:", day);
+            const mealDay = `<div class="meal-day">${day}</div>`;
+
+            const mealDayDiv = document.createElement('div');
+            mealDayDiv.innerHTML = mealDay;
+            mealsListContainer.appendChild(mealDayDiv);
+
+            // console.log("===");
+            // console.log(days[day]);
+            // console.log("===");
+            }
+
+
             // Loop over each meal type for this day
             for (const mealType in days[day]) {
-              console.log(`  ${mealType}:`);
+                console.log("test");
+                console.log("day", day);
+                console.log("mealType", mealType);
+                console.log("test");
+
+
+              const mealTypeHTML = `
+              <div class="content-row meal-content-row">
+                <label for="meal-row" style="padding-left:10px; color: orange; font-size: 1.5vh">${mealType}</label>
+                    <div name="meal-row" class="meal-row">
+                        <div class="meal-product-name" style="text-align: center;">Name</div>
+                        <div class="separator"></div>
+                        <div class="meal-nutrients">Weight [g]</div>
+                        <div class="separator"></div>
+                        <div class="meal-nutrients">Proteins [g]</div>
+                        <div class="separator"></div>
+                        <div class="meal-nutrients">Carbs [g]</div>
+                        <div class="separator"></div>
+                        <div class="meal-nutrients">Fats [g]</div>
+                        <div class="separator"></div>
+                        <div class="meal-nutrients">Kcal</div>
+                    </div>
+
+                    <div class="separator-horizontal"></div>
+
+                    <div id="${day}-${mealType}"></div>
+
+                    <div class="separator-horizontal"></div>
+
+                    <div name="meal-row" class="meal-row">
+                        <div class="meal-product-name" style="text-align: center; color:orange">TOTAL</div>
+                        <div class="separator"></div>
+                        <div class="meal-nutrients">Weight</div>
+                        <div class="separator"></div>
+                        <div class="meal-nutrients">Proteins</div>
+                        <div class="separator"></div>
+                        <div class="meal-nutrients">Carbs</div>
+                        <div class="separator"></div>
+                        <div class="meal-nutrients">Fats</div>
+                        <div class="separator"></div>
+                        <div class="meal-nutrients">Kcal</div>
+                    </div>
+                </div>
+                </div>
+              `;
+              const mealTypeDiv = document.createElement('div');
+              mealTypeDiv.innerHTML = mealTypeHTML;
+              mealsListContainer.appendChild(mealTypeDiv);
+
+
+              console.log("posilek:", `  ${mealType}:`);
+
+
+
               for (const meal of days[day][mealType]) {
+                const mealRowHTML = `
+                <div name="meal-row" class="meal-row">
+                    <div class="meal-product-name">${meal.product_name}</div>
+                    <div class="separator"></div>
+                    <div class="meal-nutrients">${meal.weight}</div>
+                    <div class="separator"></div>
+                    <div class="meal-nutrients">${meal.meal_proteins}</div>
+                    <div class="separator"></div>
+                    <div class="meal-nutrients">${meal.meal_carbs}</div>
+                    <div class="separator"></div>
+                    <div class="meal-nutrients">${meal.meal_fats}</div>
+                    <div class="separator"></div>
+                    <div class="meal-nutrients">${meal.meal_kcal}</div>
+                </div>
+                `;
+                console.log("test2");
+                console.log("day2", day);
+                console.log("mealType2", mealType);
+                console.log("test2");
+
+                const mealRowsContainer = document.getElementById(`${day}-${mealType}`);
+                const mealRowDiv = document.createElement('div');
+                mealRowDiv.innerHTML = mealRowHTML;
+                mealRowsContainer.appendChild(mealRowDiv);
+
+
                 console.log(`    ${meal.product_name}, ${meal.meal_kcal}`);
               }
             }
--- meals/static/meals/js/group-by-days.js
+++ meals/static/meals/js/group-by-days.js
@@ -1,41 +1,4 @@
 export function groupByDays(meals) {
-    // const days = {
-    //     monday: [],
-    //     tuesday: [],
-    //     wednesday: [],
-    //     thursday: [],
-    //     friday: [],
-    //     saturday: [],
-    //     sunday: []
-    // }
-    // meals.forEach(meal => {
-    //     console.log(meal.meal_day);
-    //     switch (meal.meal_day) {
-    //         case 'monday':
-    //             days['monday'].push(meal);
-    //           break;
-    //         case 'tuesday':
-    //             days['tuesday'].push(meal);
-    //           break;
-    //         case 'wednesday':
-    //             days['wednesday'].push(meal);
-    //           break;
-    //         case 'thursday':
-    //             days['thursday'].push(meal);
-    //           break;
-    //         case 'friday':
-    //             days['friday'].push(meal);
-    //           break;
-    //         case 'saturday':
-    //             days['saturday'].push(meal);
-    //           break;
-    //         case 'sunday':
-    //             days['sunday'].push(meal);
-    //           break;
-    //       }
-    // })
-    // return days;
-
     const days = {
         monday: {},
         tuesday: {},
