b81938ca75fbb7572a80c1e3dd6d37fdca6e8eb0
paka95 <patryk.pomierski@gmail.com>
2023-06-06 21:54:36 +0200

    Improved login and register form

--- main/forms.py
+++ main/forms.py
@@ -11,9 +11,9 @@ class CustomUserCreationForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-        self.fields['email'].widget.attrs={'class': 'register-field', 'placeholder': 'Enter email', 'autocomplete': False}
-        self.fields['password1'].widget.attrs={'class': 'register-field', 'placeholder': 'Enter password'}
-        self.fields['password2'].widget.attrs={'class': 'register-field', 'placeholder': 'Confirm password'}
+        self.fields['email'].widget.attrs={'class': 'email-cell', 'placeholder': 'Enter email', 'autocomplete': False}
+        self.fields['password1'].widget.attrs={'class': 'password-cell', 'placeholder': 'Enter password'}
+        self.fields['password2'].widget.attrs={'class': 'password-cell', 'placeholder': 'Confirm password'}
 
         self.fields['email'].label = False 
         self.fields['password1'].label = False
@@ -22,29 +22,12 @@ class CustomUserCreationForm(UserCreationForm):
         self.fields['password2'].help_text = None
 
 
+
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('email',)
 
-
-# class LoginForm(forms.ModelForm):
-#     password = forms.CharField(widget=forms.PasswordInput())
-#     class Meta:
-#         model = CustomUser
-#         fields =('email',)
-
-#     def __init__(self, *args, **kwargs):
-#         super(LoginForm, self).__init__(*args, **kwargs)
-#         self.fields['email'].widget.attrs={'class': 'login-field', 'placeholder': 'Enter email', 'autocomplete': False}
-#         self.fields['password'].widget.attrs={'class': 'password-field', 'placeholder': 'Enter password'}
-
-
-
-# class RegistrationFormTemplate(UserCreationForm):
-#     class Meta(UserCreationForm.Meta):
-#         model = CustomUser
-#         fields = ('email', 'password1', 'password2')
 
 
 class LoginFormTemplate(AuthenticationForm):
@@ -54,8 +37,8 @@ class LoginFormTemplate(AuthenticationForm):
 
     def __init__(self, *args, **kwargs):
         super(LoginFormTemplate, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs={'class': 'login-field', 'placeholder': 'Enter email', 'autocomplete': False}  # Apply class to the username/email field
-        self.fields['password'].widget.attrs={'class': 'login-field', 'placeholder': 'Enter password', 'autocomplete': False}
+        self.fields['username'].widget.attrs={'class': 'email-cell', 'placeholder': 'Enter email', 'autocomplete': False}  # Apply class to the username/email field
+        self.fields['password'].widget.attrs={'class': 'password-cell', 'placeholder': 'Enter password', 'autocomplete': False}
 
         self.fields['username'].label = False 
         self.fields['password'].label = False
--- main/views.py
+++ main/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from django.contrib import messages
 
 
 
@@ -21,10 +22,18 @@ class CustomLoginView(LoginView):
     template_name = 'main/login.html'
     authentication_form = LoginFormTemplate
 
+    def form_invalid(self, form):
+        messages.error(self.request, 'Invalid username or password')
+        return super().form_invalid(form)
+
 
 class CustomRegisterView(CreateView):
     template_name = 'main/register.html'
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Error in registration')
+        return super().form_invalid(form)
 
 
